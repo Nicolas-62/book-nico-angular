76b4c14cd3c343fc4c216cecbc0d599745dc445c
Nicolas-62 <droffig@hotmail.com>
2019-10-19 16:50:26 +0200

    deplacement config firebase dans un fichier non suivi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as firebase from 'firebase';
+import { firebaseConfig } from './config/firebase.config';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,6 @@ import * as firebase from 'firebase';
 export class AppComponent {
   title = 'book-nico-angular';
   constructor(){
-	  	  // Your web app's Firebase configuration
-		const firebaseConfig = {
-		  apiKey: "AIzaSyDpZLqtMeO3COrXRv1t2b5QEo0ZIVQklBc",
-		  authDomain: "book-nico-angular.firebaseapp.com",
-		  databaseURL: "https://book-nico-angular.firebaseio.com",
-		  projectId: "book-nico-angular",
-		  storageBucket: "book-nico-angular.appspot.com",
-		  messagingSenderId: "468111683132",
-		  appId: "1:468111683132:web:65a1c360518b8a4ca31d39"
-		};
 		firebase.initializeApp(firebaseConfig);
 	}
 }
